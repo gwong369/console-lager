11d6fa40df8ef46bd1359eb0ef37347553daa7cf
Merlin <daniel.schauer93@gmail.com>
2018-08-21 20:34:54 -0500

    Travis CI fixes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,30 +1,30 @@
 // var db = require("../models");
-var path = require('path');
+var path = require("path");
 
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
   //Load login page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, '../views/login.html'));
+  res.sendFile(path.join(__dirname, "../views/login.html"));
 });
 
   //Load feed page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, '../views/feed.html'));
+  res.sendFile(path.join(__dirname, "../views/feed.html"));
 });
 
   //Load profile page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, '../views/profile.html'));
+  res.sendFile(path.join(__dirname, "../views/profile.html"));
 });
 
   //Load index page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+  res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
 };
