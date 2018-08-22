2b8dbbe39ec699075ffe723eff54c1a1f67ad4ef
Merlin <daniel.schauer93@gmail.com>
2018-08-21 20:51:34 -0500

    updated html routes correctly

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,23 +8,23 @@ module.exports = function (app) {
   });
 
   //Load login page
-  app.get("/", function (req, res) {
+  app.get("/login", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/login.html"));
   });
 
   //Load feed page
-  app.get("/", function (req, res) {
+  app.get("/feed", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/feed.html"));
   });
 
   //Load profile page
-  app.get("/", function (req, res) {
+  app.get("/profile", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/profile.html"));
   });
 
   //Load index page
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
+  app.get("/create", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/create.html"));
   });
 
 };
